fcf4ab1ae3763c79be80f9558ee9452a58532939
Leonie Dusart <leonie.dusart@gmail.com>
2020-04-02 10:26:15 +0200

    Control good association + some fixing

--- jeu.js
+++ jeu.js
@@ -1,5 +1,13 @@
 function allowDrop(ev) {
-    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    if (this.id === "ombre3" && data === "dino1"){
+        ev.preventDefault();
+    } else if (this.id === "ombre1" && data === "dino2"){
+        ev.preventDefault();
+    } else if (this.id === "ombre2" && data === "dino3"){
+        ev.preventDefault();
+    };
+    // idéalement un else avec élément draggable qui "frissone"
 };
 
 function drag(ev) {
@@ -10,8 +18,6 @@ function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
     this.appendChild(document.getElementById(data));
-    console.log(data);
-    console.log(ev.target);
 };
 
 const dinosaures = Array.from(document.querySelectorAll('.dino img'));
@@ -28,3 +34,5 @@ for (let ombre of ombres) {
 };
 
 // il faudra ajouter un controle pour vérifier la bonne association !!
+
+// écrire la fonction associée au bouton pour recommencer la partie
